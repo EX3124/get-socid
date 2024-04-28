d1b15148242b4a4cf879b22f6dadcb8c001bc4e6
EX3124 <70867298+EX3124@users.noreply.github.com>
2024-04-28 19:24:03 +0800

    1

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "sudo.get.socid"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "sudo.get.socid"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(libs.material)
+}
---
+++ app/src/main/java/sudo/get/socid/MainActivity.java
@@ -0,0 +1,79 @@
+package sudo.get.socid;
+
+import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+        button.setOnClickListener(v -> {
+            if (button.getText().equals(getString(R.string.copy)))
+                ((ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE)).setPrimaryClip(ClipData.newPlainText("label", textView.getText()));
+            else {
+                Process process;
+                try {
+                    process = Runtime.getRuntime().exec("su");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                DataOutputStream dataOutputStream = new DataOutputStream(process.getOutputStream());
+                DataInputStream dataInputStream = new DataInputStream(process.getInputStream());
+                try {
+                    dataOutputStream.writeBytes("cat /sys/devices/soc0/serial_number \n");
+                    dataOutputStream.flush();
+                    String result = dataInputStream.readLine();
+                    if (result.matches("\\d+")) {
+                        textView.setText(result);
+                        textView.setTextIsSelectable(true);
+                        button.setText(R.string.copy);
+                    } else if (result.matches("0x[0-9a-fA-F]+")) {
+                        String cover = result.substring(2);
+                        textView.setText(Integer.parseInt(cover, 16));
+                        textView.setTextIsSelectable(true);
+                        button.setText(R.string.copy);
+                    }
+                } catch (IOException ignored) {
+                }
+            }
+        });
+
+        ImageButton imageButton = findViewById(R.id.github);
+        imageButton.setOnClickListener(v -> {
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.github.com/EX3124/get-socid")));
+            } catch (ActivityNotFoundException ignored) {
+            }
+        });
+    }
+}
